7823c9a0ea17b80e1dc124f7db402872894970b0
mklueh90 <marianklueh@gmail.com>
2020-05-01 11:24:53 +0200

    tried with locally built master of Quarkus

--- application/src/main/java/com/demo/application/Application.java
+++ application/src/main/java/com/demo/application/Application.java
@@ -1,11 +1,8 @@
 package com.demo.application;
 
-import io.quarkus.scheduler.Scheduled;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.enterprise.context.ApplicationScoped;
-import javax.transaction.Transactional;
-import java.time.LocalDateTime;
 
 @Slf4j
 @ApplicationScoped
