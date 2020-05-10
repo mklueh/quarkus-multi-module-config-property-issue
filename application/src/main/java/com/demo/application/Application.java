425461bc29871c17551b07e4ac13ca73e8e2dba6
mklueh90 <marianklueh@gmail.com>
2020-05-10 14:07:10 +0200

    reproducing the issue now with quarkusDev as well

--- application/src/main/java/com/demo/application/Application.java
+++ application/src/main/java/com/demo/application/Application.java
@@ -1,10 +1,11 @@
 package com.demo.application;
 
 import com.demo.application.config.TestFieldConfigProperties;
-import io.quarkus.scheduler.Scheduled;
+import io.quarkus.runtime.StartupEvent;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
 import javax.inject.Inject;
 
 @Slf4j
@@ -14,7 +15,19 @@ public class Application {
     @Inject
     TestFieldConfigProperties testFieldConfigProperties;
 
-    @Scheduled(every = "{scheduler.cron}")
+    /**
+     * Won´t work with quarkusDev
+     */
+    void onStart(@Observes StartupEvent ev) {
+        log.info(testFieldConfigProperties.getTest());
+    }
+
+    /**
+     * TODO this won´t work as well,
+     * we leave it disabled for now to verify
+     * TestFieldConfigProperties
+     */
+    //@Scheduled(every = "{scheduler.cron}")
     public void schedule() {
         log.info(testFieldConfigProperties.getTest());
     }
