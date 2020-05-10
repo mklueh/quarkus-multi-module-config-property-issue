4ba00768bb8c1a96dbf64c3bfab4219800a99136
mklueh90 <marianklueh@gmail.com>
2020-05-10 13:37:18 +0200

    upgrade to Quarkus 1.4.2.Final

--- application/src/main/java/com/demo/application/Application.java
+++ application/src/main/java/com/demo/application/Application.java
@@ -1,14 +1,22 @@
 package com.demo.application;
 
+import com.demo.application.config.TestFieldConfigProperties;
 import io.quarkus.scheduler.Scheduled;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.enterprise.context.ApplicationScoped;
-import javax.transaction.Transactional;
-import java.time.LocalDateTime;
+import javax.inject.Inject;
 
 @Slf4j
 @ApplicationScoped
 public class Application {
+
+    @Inject
+    TestFieldConfigProperties testFieldConfigProperties;
+
+    @Scheduled(every = "{scheduler.cron}")
+    public void schedule() {
+        log.info(testFieldConfigProperties.getTest());
+    }
 
 }
